9632b7289fc464b9308013cdeaa48c5e42db0188
BgBahasajerman <bgbahasajerman@gmail.com>
2025-08-27 12:41:59 +0700

    last test before turning attention to VSC, now on correct branch! (dev)

--- DataAccessLibraries/DataAccess/MockRepository.cs
+++ DataAccessLibraries/DataAccess/MockRepository.cs
@@ -5,7 +5,7 @@ namespace bgbahasajerman_DataAccessLibrary.DataAccess
         public Task<string> GetGreetingAsync()
         {
             // Hard-coded value, simulating a query result
-            return Task.FromResult("Hello from the **not so fucked up** mock repository!");
+            return Task.FromResult("Hello from the mock repository! No need to curse anymore..");
         }
     }
 }
